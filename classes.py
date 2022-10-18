201be02c78ea7537bdb79da95fac4e9ddcfd3682
David <dperellog@gmail.com>
2022-10-18 20:43:37 +0200

    Added comments

--- classes.py
+++ classes.py
@@ -1,33 +1,45 @@
+#=== IMPORTS ===#
 import random
 import languages as get
 
+#=== CLASSE TAULER ===#
 class Tauler(object):
-    tIDs = 0
+    tIDs = 0 #Propietat global que compta el número de taulells que s'han creat.
+    
+    #Constants de classe ("helpers"):
     abc = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
     nums = "0123456789"
+
+    #Constructor:
     def __init__(self, mida, vaixells=[], nomJugador='', vides=0, lang='ca'):
+        #Definir propietats base:
         self.__class__.tIDs +=1
         self.tID = self.__class__.tIDs
         self.nomJugador = nomJugador
         self.lang = lang
         self.mida = mida
-        self.matriu = self.iniciarTauler(self.mida)
-        self.llVaixells = vaixells
-        self.vaixellsActius = 0
-        self.colocarVaixells()
         self.vides = vides
         self.videsActivades = (vides > 0)
+        self.llVaixells = vaixells
+        self.vaixellsActius = 0
         self.jocAcabat = False
-    
-    def __str__(self):
-        return get.text('playground/info', self.lang, self.getID(), self.getName())
+
+        #Inicialitzar la matriu:
+        self.matriu = self.iniciarTauler(self.mida)
+
+        #Poblar la matriu:
+        self.colocarVaixells()
 
 
-    #Metodes per obtenir atributs:
+    #Metodes per obtenir atributs des de l'exterior:
     def getID(self):
         return str(self.tID)
 
+    def getName(self):
+        return self.nomJugador
+
     def getCasella(self, coords):
+        #Obtenir el contingut de la casella (retorna un objecte tipus Casella).
         return self.matriu[coords[0]][coords[1]]
 
     def getMatriu(self):
@@ -48,11 +60,7 @@ class Tauler(object):
     def restarVida(self):
         self.vides -= 1
 
-    def getName(self):
-        return self.nomJugador
 
-    def partidaGuanyada(self):
-        return self.jocAcabat
 
     #Metodes del tauler:
     def iniciarTauler(self, mida):
@@ -79,12 +87,22 @@ class Tauler(object):
                 if dev:
                     print(columna.printDev(), end=" ")
                 else:
-                    
                         print(columna, end=" ")
             print()
             f+=1
 
     def submatriuBuida(self, coords, orientacio, tamany):
+    #Aquesta funció comprova si l'àrea on s'ha de situar el vaixell està buida o no.
+        #Funció que ajuda a comprovar si l'àrea seleccionada conté tot aigua.
+        def comprovarAigua(topLeft, bottomRight):
+            aigua = True
+            for i in range(topLeft[0],bottomRight[0]+1):
+                    for j in range(topLeft[1],bottomRight[1]+1):
+                        if self.matriu[i][j].getCasella() != "~":
+                            aigua = False #Si hi ha alguna coordenada que no sigui aigua, el valor és fals.
+            return aigua
+
+        #Codi principal de la funció:
         try:
             if orientacio == 'v':
                 topLeft = (coords[0]-1,coords[1]-1)
@@ -92,24 +110,23 @@ class Tauler(object):
             else:
                 topLeft = (coords[0]-1,coords[1]-1)
                 bottomRight = (coords[0]+1,coords[1]+tamany+1)
-
-            return self.comprovarAigua(topLeft, bottomRight)
+            return comprovarAigua(topLeft, bottomRight)
                 
         except IndexError:
+            #Si el vaixell surt dels límits del tauler, retornar fals.
             return False
     
-    def comprovarAigua(self, topLeft, bottomRight):
-        aigua = True
-        for i in range(topLeft[0],bottomRight[0]+1):
-                for j in range(topLeft[1],bottomRight[1]+1):
-                    if self.matriu[i][j].getCasella() != "~":
-                        aigua = False #Si hi ha alguna coordenada que no sigui aigua, el valor és fals.
-        return aigua
     
+    #Funció que donada la llista de vaixells, els col·loca al taulell.
     def colocarVaixells(self):
+
         for vaixell in self.llVaixells:
-            for i in range(vaixell['quantitat']):
+
+            #Inserta tants vaixells iguals com la quantitat d'aquests.
+            for q in range(vaixell['quantitat']):
                 vaixellColocat = False
+
+                #Intenta col·locar el vaixell fins que estigui col·locat
                 while not(vaixellColocat):
                     #Generar coordenades aleatories:
                     orientacio = random.choice(['v','h'])
@@ -122,34 +139,20 @@ class Tauler(object):
                     if self.submatriuBuida(coords, orientacio, vaixell['mida']):
                         Vaixell(vaixell['mida'], get.text(vaixell['nom'], self.lang)).generarVaixell(self,orientacio,coords)
                         vaixellColocat = True
+
+                #Comptabilitza el vaixell dins del comptador de vaixells no enfonsats.        
                 self.vaixellsActius += 1
 
+    #Funció que comptabilitza el nombre de vaixells que resten amb vida.
     def vaixellEnfonsat(self):
         if self.vaixellsActius > 0:
             self.vaixellsActius -= 1
 
+        #Si s'ha enfonsat tota la flota, mostrar que s'ha guanyat.
         if self.vaixellsActius == 0:
             self.jocAcabat = True
-        
-    def tret(self, coords):
-        coords = tuple([int(coords[0]), self.abc.index(coords[1])])
 
-        casella = self.getCasella(coords)
-        if not(casella.casellaOberta()):
-            if casella.teVaixell():
-                vaixell = casella.getCasella()
-                vaixellMort = vaixell.tocat()
-                if vaixellMort:
-                    self.vaixellEnfonsat()
-                    get.text('playground/sink', self.lang, vaixell.getName().upper(), mostrar=True)
-                else:
-                    get.text('playground/touched', self.lang, mostrar=True)
-            else:
-                get.text('playground/water', self.lang, mostrar=True)
-                self.restarVida()
-        else:
-            get.text('errors/cordOpened', self.lang, mostrar=True)
-
+    #Funció que valida que la coordenada entrada existeixi dins de tauler.
     def comprovarCoordenades(self, coord):
         mida = self.mida
         numCorrecte = False
@@ -163,15 +166,47 @@ class Tauler(object):
                 llCorrecte = True
         
         return numCorrecte and llCorrecte
+
+    #Funció que llança un tret a una coordenada donada (prèviament verificada).    
+    def tret(self, coords):
+        coords = tuple([int(coords[0]), self.abc.index(coords[1])])
+
+        #Obté l'objecte Casella de la coordenada.
+        casella = self.getCasella(coords)
+
+        if not(casella.casellaOberta()):
+            if casella.teVaixell():
+
+                #Obté l'objecte vaixell de la casella.
+                vaixell = casella.getCasella()
+                #Redueix la vida del vaixell.
+                vaixellMort = vaixell.tocat()
+                #Si la vida del vaixell és 0, s'ha enfonsat.
+                if vaixellMort:
+                    #Notifica l'objecte Tauler que s'ha enfonsat un vaixell.
+                    self.vaixellEnfonsat()
+                    get.text('playground/sink', self.lang, vaixell.getName().upper(), mostrar=True)
+                else:
+                    get.text('playground/touched', self.lang, mostrar=True)
+            else:
+                get.text('playground/water', self.lang, mostrar=True)
+                self.restarVida()
+        else:
+            get.text('errors/cordOpened', self.lang, mostrar=True)
     
+    #Funció que entra en bucle mentre la partida està en primer pla.
     def jugar(self):
+        #Retorna True si la partida s'ha acabat (jugador ha guanyat o s'ha mort).
+        #Retorna False si el jugador vol tornar al menú.
+
         partidaActiva = True
         
-        get.text('playground/info', self.lang, self.getID(), self.getName(), mostrar=True)
+        get.text('playground/info', self.lang, self.tID, self.getName(), mostrar=True)
         get.text('playground/exitPlaygroundInfo', self.lang, mostrar=True)
 
         while partidaActiva:
-            if self.partidaGuanyada():
+            #Si el joc ha acabat, notifica-ho al jugador i al programa principal.
+            if self.jocAcabat:
                 get.text('playground/winMessage', self.lang, mostrar=True)
                 self.mostraTauler(True)
                 return True
@@ -179,6 +214,8 @@ class Tauler(object):
                 print(get.text('playground/deathMessage', self.lang)+'\n')
                 self.mostraTauler(True)
                 return True
+
+            #Si no s'ha acabat el joc, demana al jugador noves coordenades i fes el tret.
             else:
                 if self.videsActives():
                     get.text('playground/remaining', self.lang, self.videsRestants(), mostrar=True)
@@ -194,15 +231,15 @@ class Tauler(object):
                         self.tret(casella)
     
 
-
-
+#=== CLASSE VAIXELL ===#
 class Vaixell(object):
     def __init__(self, mida, nom=''):
         self.mida = mida
         self.vides = mida-1
-        self.visual = 'X'
+        self.visual = 'X' #Retorna com s'ha de mostrar el vaixell.
         self.name = nom 
     
+    #Funció que actualitza el contingut de les instànices Casella on ha d'anar el vaixell.
     def generarVaixell(self, tauler, orientacio, coord):
         if orientacio == 'h':
             for c in range(self.mida):
@@ -211,25 +248,30 @@ class Vaixell(object):
             for c in range(self.mida):
                 tauler.getCasella((coord[0]+c,coord[1])).setCasella(self)
     
+    #Retorna com s'ha de mostrar el vaixell.
     def getVisual(self):
         return self.visual
 
     def getName(self):
         return self.name
 
+    #Si el jugador ha trobat el vaixell, actualitza les vides.
     def tocat(self):
         if self.vides > 0:
             self.vides -= 1
             return False
         else:
+            #Si el vaixell s'ha enfonsat, actualitza la visual.
             self.visual = "#"
             return True
 
+#=== CLASSE CASELLA ===#
 class Casella(object):
     def __init__(self):
         self.content = '~'
         self.visible = False
     
+    #Modifica el que ha de retornar l'objecte quan es printa.
     def __str__(self):
         if self.visible:
             if isinstance(self.content, Vaixell):
@@ -239,24 +281,26 @@ class Casella(object):
         else:
             return "·"
 
-    def __getitem__(self, items):
-        return str(self)
-
+    #Funció utilitzada per mostrar el contingut de la casella encara que no s'hagi obert.
     def printDev(self):
         if isinstance(self.content, Vaixell):
             return self.content.getVisual()
         else:
             return self.content
 
+    #Modificar el contingut de la casella.
     def setCasella(self, content):
         self.content = content
     
+    #Retornar el contingut de la casella.
     def getCasella(self):
         return self.content
 
+    #Retorna si la casella té un objecte vaixell o no.
     def teVaixell(self):
         return isinstance(self.content, Vaixell)
 
+    #Comprova si la casella ja s'ha obert. Si no, l'obre.
     def casellaOberta(self):
         if not(self.visible):
             self.visible = True
--- main.py
+++ main.py
@@ -1,6 +1,9 @@
+#=== IMPORTS ===#
+#Importar les classes i el diccionari de llenguatges.
 from classes import *
 import languages as get
 
+#=== CONSTANTS ===#
 MIDATAULELL = 10
 FLOTA = [{'nom':'fleet/four','mida':4,'quantitat':1},
         {'nom':'fleet/three','mida':3,'quantitat':2},
@@ -8,13 +11,15 @@ FLOTA = [{'nom':'fleet/four','mida':4,'quantitat':1},
         {'nom':'fleet/one','mida':1,'quantitat':4}]
 VIDES = 50
 LLENGUATGES = ('ca', 'es', 'en')
-lang = ''
-accio = 0
 
+#=== VARIABLES ===#
+lang = ''
 jocsActius = {}
 taulerActual = 0
 
-#Funcions:
+#=== FUNCIONS ===#
+
+#Funció que mostra el menú i permet al jugador seleccionar.
 def mostraMenu():
     accio = 0
     get.text('menu/options',lang, mostrar=True)
@@ -25,11 +30,13 @@ def mostraMenu():
                 accio = 0
         else:
             accio = 0
-    return int(accio)
+    return accio
 
+#Funció que recull propietats de la partida i inicialitza un objecte Taulell.
 def nouTauler(MIDATAULELL, FLOTA, VIDES):
     global jocsActius
 
+    #Obtenir l'idioma del taulell.
     sortir = False
     while not(sortir):
         langTauler = input(get.text('playground/selectLang', lang, lang.upper(), LLENGUATGES)).lower()
@@ -39,6 +46,7 @@ def nouTauler(MIDATAULELL, FLOTA, VIDES):
             langTauler = lang
             sortir = True
 
+    #Obtenir si el jugador vol jugar amb vides o sense.
     sortir = False
     while not(sortir):
         vides = input(get.text('playground/playWLives', langTauler))
@@ -47,13 +55,15 @@ def nouTauler(MIDATAULELL, FLOTA, VIDES):
         if (langTauler in ('en')) and (vides.upper() in "YN"):
             sortir = True
 
-    
+    #Inicialitzar el taulell.
     nomTauler = input(get.text('playground/enterName', langTauler))
     tauler = Tauler(MIDATAULELL,FLOTA,nomTauler,(0 if vides.upper() == 'N' else VIDES), langTauler)
     jocsActius[tauler.getID()] = tauler
+    
+    #Retornar l'ID del taulell
     return str(tauler.getID())
 
-
+#Funció que permet cambiar l'idioma general del programa (el menú).
 def canviarIdioma():
     global lang
     lang = ''
@@ -62,41 +72,53 @@ def canviarIdioma():
         if lang not in LLENGUATGES:
             lang = ''
 
-#- Programa principal: -#
+#=== PROGRAMA PRINCIPAL ===#
+#Permet al jugador establir un idioma pel menú.
 canviarIdioma()
 
 get.text('menu/welcome', lang, mostrar=True)
 
-while accio != 4:
+#Realitzar l'acció associada a l'opció del menú:
+accio = '0'
+while accio != '4':
     accio = mostraMenu()
     
-    if accio == 1:
+    #1->Iniciar nova partida:
+    if accio == '1':
 
         taulerActual = nouTauler(MIDATAULELL, FLOTA, VIDES)
 
         resultat = jocsActius[taulerActual].jugar()
+        
+        #Si s'ha acabat la partida, eliminar el taulell de la llista.
         if resultat:
             del jocsActius[taulerActual]
             
-
-    if accio == 2:
+    #2->Llistar i recuperar partida anterior:
+    if accio == '2':
+        #Mostrar les partides actuals:
         if len(jocsActius) > 0:
             print("\n"+get.text('menu/activePlaygrounds', lang)+"\n")
             for tauler in jocsActius:
                 get.text('playground/info', lang, tauler, jocsActius[tauler].getName(), mostrar=True)
             
+            #Permetre al jugador seleccionar el taulell per ID:
             taulerCorrecte = False
             while not(taulerCorrecte):
                 taulerID = input(get.text('menu/insertID', lang))
                 if taulerID in jocsActius.keys():
-                    taulerActual = taulerID
+                    taulerActual = taulerID #Actualitzar el taulell actual.
                     taulerCorrecte = True
-                    
+            
+            #Jugar al taulell actual:
             resultat = jocsActius[taulerActual].jugar()
+            
+            #Si s'ha acabat la partida, eliminar el taulell de la llista.
             if resultat:
                 del jocsActius[taulerActual]
         else:
             get.text('errors/playgroundsNotFound', lang, mostrar=True)
     
-    if accio == 3:
+    #3->Canviar l'idioma principal:
+    if accio == '3':
         canviarIdioma()
